d2abf921cf17227cfea91615d06daba2d17aeaec
kainoabrabo <kubaldob@ucsc.edu>
2023-04-26 20:03:25 -0700

    setup .env file (#23)

--- pages/firebase.js
+++ pages/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfJowBQSMx16JMwZWKgNBd5IgxFnkKcdM",
-  authDomain: "nextjs-ff.firebaseapp.com",
-  projectId: "nextjs-ff",
-  storageBucket: "nextjs-ff.appspot.com",
-  messagingSenderId: "239096580979",
-  appId: "1:239096580979:web:ea4db5ea4c06e3f8635306",
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
 };
 
 // Initialize Firebase
