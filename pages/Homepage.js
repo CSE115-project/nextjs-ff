1cf5fcf44eb3c99605de9c5501cd1d7f58833db0
kainoabrabo <kubaldob@ucsc.edu>
2023-04-26 23:46:48 -0700

    set default location to SF (#22)

--- pages/Homepage.js
+++ pages/Homepage.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Button from "@mui/joy/Button";
 import { getAuth, signOut } from "firebase/auth";
 import Map from "./Map";
+import { useState } from "react";
 
 export default function Homepage({ user }) {
   // Authentication
@@ -14,17 +15,18 @@ export default function Homepage({ user }) {
 
   // Google Maps
   // default: San Francisco
-  const location = {
+  const defLocation = {
     lat: 37.7749,
     lng: -122.4194,
   };
+  const [location, setLocation] = useState(defLocation);
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <Button onClick={handleSignOut} sx={{ mt: 1 }}>
         Sign Out
       </Button>
-      <Map></Map>
+      <Map location={location}></Map>
     </div>
   );
 }
