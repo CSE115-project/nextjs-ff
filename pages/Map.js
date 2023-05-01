694adff5b6cf9a9556c964f1a3bafac68ab450a2
kainoabrabo <kubaldob@ucsc.edu>
2023-04-30 20:30:08 -0700

    add location marker for user (#25)

--- pages/Map.js
+++ pages/Map.js
@@ -1,8 +1,12 @@
 import GoogleMapReact, { Marker } from "google-map-react";
 
 export default function Map({ location }) {
+  const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
   let center = { lat: location.lat, lng: location.lng };
   let zoom = 11;
+
+  console.log("location:", location);
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
@@ -10,6 +14,8 @@ export default function Map({ location }) {
         center={center}
         zoom={zoom}
       >
+        <AnyReactComponent lat={location.lat} lng={location.lng} text="CURRENT LOCATION" />
+        {/* {location && <Marker postion={location} />} */}
       </GoogleMapReact>
     </div>
   );
