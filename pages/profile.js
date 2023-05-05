859304e81d056bcefcc996a81cf9c74937e9b761
Huy Nguyen <duchuyn0411@gmail.com>
2023-05-05 00:09:10 -0700

    set profile API route (#27)

--- pages/profile.js
+++ pages/profile.js
@@ -1,8 +1,17 @@
+import Link from 'next/link';
+import Button from "@mui/joy/Button";
+
 export default function Profile() 
 {
     return (
         <div className="userProfile">
             <h1>user profile</h1>
+            
+            {/* Home button */}
+            <Link href = "/Homepage">
+                <Button>Home</Button>
+            </Link>
+            
         </div>
-    )
+    );
 }
