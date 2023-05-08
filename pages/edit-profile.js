5e91d61cc8df68f6e11ca8612a3dfd25c17c548d
Edison Chen <echen70@ucsc.edu>
2023-05-08 16:28:09 -0700

    minor ui fixes

--- pages/edit-profile.js
+++ pages/edit-profile.js
@@ -12,25 +12,49 @@ import Sheet from "@mui/joy/Sheet";
 import Stack from "@mui/joy/Stack";
 import Typography from "@mui/joy/Typography";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-function addInputUntiLimit(inputVal, setField, limit)
-{
-    if(inputVal.length <= limit) {
-      setField(inputVal);
-    }
+function addInputUntiLimit(inputVal, setField, limit) {
+  if (inputVal.length <= limit) {
+    setField(inputVal);
+  }
 }
 
 export default function MyProfile() {
+  const router = useRouter();
+  // userData is a string that contains the UID
+  const { userData } = router.query;
+
   const [firstName, setFirstName] = React.useState("");
   const [lastName, setLastName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [bio, setBio] = React.useState("");
   const [selectedFile, setSelectedFile] = React.useState(null);
+  const [field, setField] = React.useState("");
+  const [content, setContent] = React.useState("");
+
+  // function to edit the database, this function can even serve as to adding
+  // to the database; althought I'm not 100% sure on if it'll work for adding lists
+  const editData = async () => {
+    try {
+      const response = await fetch("/api/editUser", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ uid: userData, field, content }),
+      });
+
+      console.log("RESPONSE", response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   //cancel function
   //TODO: change default values from Firestore
   const handleCancel = () => {
-    setFirstName(""); 
+    setFirstName("");
     setLastName("");
     setEmail("");
     setBio("");
@@ -46,23 +70,26 @@ export default function MyProfile() {
   const handleFirstNameChange = (event) => {
     const inputVal = event.target.value;
     addInputUntiLimit(inputVal, setFirstName, maxFirstNameChar);
-  }
+  };
 
   const handleLastNameChange = (event) => {
     const inputVal = event.target.value;
     addInputUntiLimit(inputVal, setLastName, maxLastNameChar);
-  }
+  };
 
   const handleEmailChange = (event) => {
     const inputVal = event.target.value;
     addInputUntiLimit(inputVal, setEmail, maxEmailChar);
-  }
+  };
 
   const handleBioChange = (event) => {
-      const inputVal = event.target.value;
-      addInputUntiLimit(inputVal, setBio, maxBioChar);
+    const inputVal = event.target.value;
+    addInputUntiLimit(inputVal, setBio, maxBioChar);
+    setField("bio");
+    setContent(bio);
+    // console.log("bio is:", bio);
   };
-  
+
   //set remaining bio characters
   const remainingChar = maxBioChar - bio.length;
 
@@ -70,35 +97,49 @@ export default function MyProfile() {
     const file = event.target.files[0];
     setSelectedFile(file);
   };
-  
+
   //upload function
   const handleUpload = () => {
-      if (selectedFile) {
-          // Perform the upload logic here
-          console.log("Uploading file:", selectedFile);
-      }
+    if (selectedFile) {
+      // Perform the upload logic here
+      console.log("Uploading file:", selectedFile);
+    }
   };
-    
+
+  const handleHome = (event) => {
+    event.preventDefault();
+    router.push({
+      pathname: "/",
+      query: { userData },
+    });
+  };
+
+  const handleProfile = (event) => {
+    event.preventDefault();
+    router.push({
+      pathname: "/profile",
+      query: { userData },
+    });
+  };
+
   return (
     <Sheet>
       <Stack direction="row" alignItems="center" spacing={0}>
-        {/* Home Button */}
-        <Link href="/">
-          <Button sx={{ ml: 3 }}>Home</Button>
-        </Link>
+        <Button onClick={handleHome} sx={{ mt: 1 }}>
+          Home
+        </Button>
 
-        {/* Profile Button */}
-        <Link href = "/profile">
-          <Button>Profile</Button>
-        </Link>
+        <Button onClick={handleProfile} sx={{ mt: 1 }}>
+          Profile
+        </Button>
 
-        <Typography level="h1" fontSize="xl2">
+        <Typography level="h1" fontSize="xl2" sx={{ ml: 5 }}>
           My profile
         </Typography>
       </Stack>
       <Sheet
         sx={{
-          maxWidth: 1200,
+          maxWidth: "75%",
           width: "100%",
           mx: "auto",
         }}
@@ -129,12 +170,20 @@ export default function MyProfile() {
           <Box sx={{ display: { xs: "contents", sm: "flex" }, gap: 2 }}>
             <FormControl sx={{ flex: 1 }}>
               <FormLabel sx={{ display: { sm: "none" } }}>First name</FormLabel>
-              <Input value={firstName} onChange={handleFirstNameChange} placeholder="First Name" />
+              <Input
+                value={firstName}
+                onChange={handleFirstNameChange}
+                placeholder="First Name"
+              />
             </FormControl>
 
             <FormControl sx={{ flex: 1 }}>
               <FormLabel sx={{ display: { sm: "none" } }}>Last name</FormLabel>
-              <Input value={lastName} onChange={handleLastNameChange} placeholder="Last Name" />
+              <Input
+                value={lastName}
+                onChange={handleLastNameChange}
+                placeholder="Last Name"
+              />
             </FormControl>
           </Box>
 
@@ -174,8 +223,8 @@ export default function MyProfile() {
               sx={{ "--Avatar-size": "64px" }}
             />
             <Box sx={{ marginTop: 3 }}>
-                <input type="file" accept="image/*" onChange={handleFileChange} />
-                <Button onClick={handleUpload}>Upload</Button>
+              <input type="file" accept="image/*" onChange={handleFileChange} />
+              <Button onClick={handleUpload}>Upload</Button>
             </Box>
           </Box>
 
@@ -187,9 +236,15 @@ export default function MyProfile() {
           </Box>
 
           <Box>
-            <Textarea minRows={4} value={bio} onChange={handleBioChange} maxLength={maxBioChar} sx={{ mt: 1.5 }} />
+            <Textarea
+              minRows={4}
+              value={bio}
+              onChange={handleBioChange}
+              maxLength={maxBioChar}
+              sx={{ mt: 1.5 }}
+            />
             <FormHelperText sx={{ mt: 0.75, fontSize: "xs" }}>
-                {remainingChar} characters left
+              {remainingChar} characters left
             </FormHelperText>
           </Box>
 
@@ -204,12 +259,19 @@ export default function MyProfile() {
             }}
           >
             {/* TODO: Add cancel functionality for files*/}
-            <Button variant="outlined" color="neutral" size="sm" onClick={handleCancel}>
+            <Button
+              variant="outlined"
+              color="neutral"
+              size="sm"
+              onClick={handleCancel}
+            >
               Cancel
             </Button>
 
             {/* TODO: Add save functionality */}
-            <Button size="sm">Save</Button>
+            <Button size="sm" onClick={editData}>
+              Save
+            </Button>
           </Box>
         </Box>
       </Sheet>
