0f7e0ba01a9c807df35bc89f89091bc0dd46939d
kainoabrabo <kubaldob@ucsc.edu>
2023-05-09 22:42:35 -0700

    refresh profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -6,39 +6,6 @@ import { useState } from "react";
 
 export default function Profile() {
   const router = useRouter();
-  // userData is a string that contains the UID
-  const { userData } = router.query;
-  console.log("uid:", userData);
-
-  const [userName, setUserName] = useState("");
-
-  // this function fetches the data from the database...
-  // need to set variables according to what is needed to be displayed
-  const fetchData = async () => {
-    try {
-      // const response = await fetch("/api/getUser", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({ uid: userData }),
-      // });
-
-      // console.log("RESPONSE", response);
-
-      // const data = await response.json();
-
-      // console.log("DATA:", data);
-
-      // const useData = data.data;
-      // // do something with the data here
-      // setUserName(useData.name);
-      // // add additional variables being set here depending on what will be displayed
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  fetchData();
 
   const handleHome = (event) => {
     event.preventDefault();
@@ -67,7 +34,6 @@ export default function Profile() {
       </Button>
 
       <h1>User Profile</h1>
-      <h2> {userName} </h2>
 
       <Avatar />
       {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" /> */}
