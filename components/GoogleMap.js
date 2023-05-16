854f952f7678a1397b9b364b2e746a4a212f98db
kainoabrabo <kubaldob@ucsc.edu>
2023-05-15 23:46:29 -0700

    set  user current location (#36)

--- components/GoogleMap.js
+++ components/GoogleMap.js
@@ -15,18 +15,44 @@ export default function GoogleMap() {
     const loader = new Loader({
       apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API,
       version: "weekly",
-    //   ...additionalOptions,
+      //   ...additionalOptions,
     });
 
     loader.load().then(async () => {
-        const {google} = window;
+      const { google } = window;
       const { Map } = await google.maps.importLibrary("maps");
 
-      const map = new Map(mapRef.current, {
-        center: { lat: defLocation.lat, lng: defLocation.lng },
-        zoom: 8,
-      });
+      // Get the user's current location
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const currentLocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          // Create a new map centered on the user's current location
+          const map = new Map(mapRef.current, {
+            center: currentLocation,
+            zoom: 8,
+          });
+
+          // Add a marker for the user's current location
+          new google.maps.Marker({
+            position: currentLocation,
+            map,
+          });
+        });
+      } else {
+        const map = new Map(mapRef.current, {
+          center: { lat: defLocation.lat, lng: defLocation.lng },
+          zoom: 11,
+        });
+      }
     });
   }, []);
-  return <div ref={mapRef} style={{ height: "400px" }} >GoogleMap</div>;
+  return (
+    <div ref={mapRef} style={{ height: "90%", width: "100%" }}>
+      GoogleMap
+    </div>
+  );
 }
