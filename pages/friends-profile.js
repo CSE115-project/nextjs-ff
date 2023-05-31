505318bdb130d192bd5f5be8bdc50330aea59e7b
Huy Nguyen <dnguy209@ucsc.edu>
2023-05-31 00:08:40 -0700

    updating friend's page

--- pages/friends-profile.js
+++ pages/friends-profile.js
@@ -3,7 +3,7 @@ import Avatar from "@mui/joy/Avatar";
 import Box from "@mui/joy/Box";
 import Sheet from "@mui/joy/Sheet";
 import Stack from "@mui/joy/Stack";
-import { Tab, Tabs, TabList, tabClasses, TabPanel } from "@mui/joy";
+import { Tab, Tabs, TabList, tabClasses, TabPanel, Divider } from "@mui/joy";
 import Typography from "@mui/joy/Typography";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -20,6 +20,11 @@ import {
   arrayUnion,
 } from "firebase/firestore";
 import Input from "@mui/joy/Input";
+import * as React from 'react';
+import List from '@mui/joy/List';
+import ListItem from '@mui/joy/ListItem';
+import ListItemDecorator from '@mui/joy/ListItemDecorator';
+
 
 /*
 const userObj = {
@@ -41,22 +46,26 @@ export default function Profile({ user }) {
 
   // Routes -------------------------------------------------
   // initialize all fields and their according set methods
+  
+  // set route to go back to home from profile page
   const handleHome = (event) => {
     if (event.cancelable) event.preventDefault();
     router.push("/");
   };
 
+  // set route to go to edit-profile from profile page
   const handleEditProfile = (event) => {
     if (event.cancelable) event.preventDefault();
     router.push("/edit-profile");
   };
 
-  // function to retrieve the UserData from the database
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // set route to go to friends-profile from profile page
+  const handleFriendProfile = (event) => {
+    if (event.cancelable) event.preventDefault();
+    router.push("/friends-profile");
+  };
 
+  // function to retrieve the UserData from the database
   const fetchData = async () => {
     try {
       const docRef = doc(db, "users", user.uid);
@@ -65,6 +74,7 @@ export default function Profile({ user }) {
       if (docRes.exists()) {
         const data = docRes.data();
         setUserData(data);
+        // createFriendList(data.friends);
       } else {
         console.error("User Not Found");
       }
@@ -74,6 +84,44 @@ export default function Profile({ user }) {
   };
 
   console.log("p/profile userData:", userData);
+
+  // Adding search friends functionality ----------------------------------------------------------
+
+  // Handle search query input and button click
+  const [searchQuery, setSearchQuery] = useState("");
+  const [friendsList, setFriendsList] = useState([]);
+
+  const handleAddFriend = async (event) => {
+    // prevent reload on click
+    if (event.cancelable) event.preventDefault();
+
+    // Search and Get User with email matching the search.
+    // Since only one email is associated with each user,
+    // the return doc should only contain one entry
+    const db = getFirestore();
+    const q = query(collection(db, "users"), where("email", "==", searchQuery));
+    const qSnap = await getDocs(q);
+
+    // If empty, there are no users with that email
+    if (qSnap.size < 1) {
+      console.error("No User email matches");
+      return;
+    }
+
+    // Get the new friend's ID
+    const newFriendId = qSnap.docs[0].id;
+
+    // Update Friends list of User by appending new_friend_id
+    const docRef = doc(db, "users", user.uid);
+
+
+    if (newFriendId) {
+      await updateDoc(docRef, { friends: arrayUnion(newFriendId) });
+      console.log("Friend added");
+      await fetchData();
+      await createFriendList();
+    }
+  };
 
   // List friends ----------------------------------------------------------
 
@@ -89,22 +137,34 @@ export default function Profile({ user }) {
 
   const createFriendList = async () => {
     const db = getFirestore();
-    const { friends } = userData;
+    // const { friends } = userData;
 
     try {
-      const promises = friends.map((friendId) => getFriend(db, friendId));
+      const promises = userData.friends ? userData.friends.map((friendId) => getFriend(db, friendId)) : [];
+
       const friendData = await Promise.all(promises);
-      const list = [...friendData];
+      // const list = [...friendData];
       // friendsList.push(...friendData);
-      // setFriendsList(list);
-      console.log("friend list:", list);
+      setFriendsList(friendData);
+      console.log("friend list:", friendData);
       // TODO: save list to global list, and map the results in the friends list tab
     } catch (error) {
       console.log("Error retrieving friend data:", error);
     }
   };
 
-  createFriendList();
+  useEffect(() => {
+    if (userData && userData.friends) {
+      createFriendList(userData.friends);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userData]);
+
+  useEffect(() => {
+    fetchData();
+    createFriendList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (!userData) {
     return (
@@ -157,7 +217,7 @@ export default function Profile({ user }) {
                 borderRadius: "20px",
               }}
             >
-              {/* need to test with real avatar */}
+              {/* User's Avatar */}
               <Avatar
                 sx={{ height: "128px", width: "128px", margin: "auto" }}
                 src={userData.image || ""}
@@ -181,6 +241,38 @@ export default function Profile({ user }) {
                 {userData.bio || "Bio.."}
               </Typography>
             </Box>
+
+            <Typography component="h4" sx={{ py: 1, margin: "auto" }}>
+              Add Friend
+            </Typography>
+
+            {/* stack for formatting purposes */}
+            <Stack
+              direction="row"
+              alignItems="center"
+              justifyContent="center"
+              spacing={1}
+              sx={{ flexWrap: "wrap" }}
+            >
+              {/* Form for inputting friend's email */}
+              <form onSubmit={handleAddFriend}>
+                <Input
+                  type="email"
+                  placeholder="email"
+                  sx={{
+                    width: { xs: "100%", sm: 400 },
+                    display: { xs: "block flex", sm: "flex" },
+                  }}
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  // Button to add a friend
+                  endDecorator={<Button type="submit">Add</Button>}
+                />
+              </form>
+
+              {/* Render the matching users */}
+            </Stack>
+
             <Tabs aria-label="tabs" defaultValue={0}>
               <TabList
                 variant="plain"
@@ -216,14 +308,45 @@ export default function Profile({ user }) {
               {/* TabPanel for User's friends list */}
               <TabPanel value={1} sx={{ p: 2 }}>
                 <div>
-                  <h1>List of Friends</h1>
-                  {/* TODO */}
+                  <List
+                    variant="outlined"
+                    sx={{
+                      bgcolor: 'background.body',
+                      minWidth: 240,
+                      borderRadius: 'sm',
+                      boxShadow: 'sm',
+                      '--ListItemDecorator-size': '48px',
+                      '--ListItem-paddingLeft': '1.5rem',
+                      '--ListItem-paddingRight': '1rem',
+                    }}
+                  >
+                    {friendsList.map((friend, index) => (
+                      <React.Fragment key={index}>
+                        <Button variant="plain"
+                          sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', width: '100%', padding: 0 }}
+                          component="li"
+                          disableRipple
+                          // route to friends-profile page
+                          onClick={handleFriendProfile}>
+                          <ListItem key={index}>
+                            <ListItemDecorator sx={{ alignSelf: 'flex-start' }}>
+                              <Avatar size="sm" src="/static/images/avatar/1.jpg" />
+                            </ListItemDecorator>
+                            <Typography color="black" sx={{ fontWeight: 'normal', marginLeft: '0.5rem' }}>
+                              {friend}
+                            </Typography>
+                          </ListItem>
+                        </Button>
+                        {index !== friendsList.length - 1 && <Divider />}
+                      </React.Fragment>
+                    ))}
+                  </List>
                 </div>
               </TabPanel>
             </Tabs>
           </Sheet>
         </Sheet>
-      </div>
+      </div >
     );
   }
 }
