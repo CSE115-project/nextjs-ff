b60556ef51b1685337c07d21c1a088a629ff2476
Juan Aguenza <juan.aguenza@gmail.com>
2023-06-04 23:55:12 -0700

    Profile page that redirects to friends profiles properly

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,17 @@
+import Button from "@mui/joy/Button";
+import Avatar from "@mui/joy/Avatar";
+import Box from "@mui/joy/Box";
+import Sheet from "@mui/joy/Sheet";
+import Stack from "@mui/joy/Stack";
+import { Tab, Tabs, TabList, tabClasses, TabPanel, Divider } from "@mui/joy";
+import Typography from "@mui/joy/Typography";
 import { useRouter } from "next/router";
-import { useEffect, useState, Fragment } from "react";
-import { db } from "../firebase";
-import { Loader } from "@googlemaps/js-api-loader";
+import { useEffect, useState } from "react";
+import { firebase } from "../firebase";
 import {
   getDoc,
   getDocs,
+  getFirestore,
   doc,
   query,
   collection,
@@ -12,25 +19,14 @@ import {
   updateDoc,
   arrayUnion,
 } from "firebase/firestore";
-import {
-  Tab,
-  Tabs,
-  TabList,
-  tabClasses,
-  TabPanel,
-  Divider,
-  Input,
-  Alert,
-  List,
-  ListItem,
-  ListItemDecorator,
-  Button,
-  Avatar,
-  Box,
-  Sheet,
-  Stack,
-  Typography,
-} from "@mui/joy";
+import Input from "@mui/joy/Input";
+import Alert from '@mui/joy/Alert';
+import * as React from 'react';
+import List from '@mui/joy/List';
+import ListItem from '@mui/joy/ListItem';
+import ListItemDecorator from '@mui/joy/ListItemDecorator';
+import { async } from "@firebase/util";
+
 
 /*
 const userObj = {
@@ -47,11 +43,12 @@ const userObj = {
 
 export default function Profile({ user }) {
   const router = useRouter();
+  const db = getFirestore(firebase);
   const [userData, setUserData] = useState({});
 
   // Routes -------------------------------------------------
   // initialize all fields and their according set methods
-
+  
   // set route to go back to home from profile page
   const handleHome = (event) => {
     if (event.cancelable) event.preventDefault();
@@ -99,6 +96,7 @@ export default function Profile({ user }) {
     // Search and Get User with email matching the search.
     // Since only one email is associated with each user,
     // the return doc should only contain one entry
+    const db = getFirestore();
     const q = query(collection(db, "users"), where("email", "==", searchQuery));
     const qSnap = await getDocs(q);
 
@@ -118,6 +116,7 @@ export default function Profile({ user }) {
 
     // Update Friends list of User by appending new_friend_id
     const docRef = doc(db, "users", user.uid);
+
 
     if (newFriendId) {
       await updateDoc(docRef, { friends: arrayUnion(newFriendId) });
@@ -163,12 +162,15 @@ export default function Profile({ user }) {
   };
 
   const createFriendList = async () => {
+    const db = getFirestore();
+    // const { friends } = userData;
+
     try {
-      const promises = userData.friends
-        ? userData.friends.map((friendId) => getFriend(db, friendId))
-        : [];
+      const promises = userData.friends ? userData.friends.map((friendId) => getFriend(db, friendId)) : [];
 
       const friendData = await Promise.all(promises);
+      // const list = [...friendData];
+      // friendsList.push(...friendData);
       setFriendsList(friendData);
       console.log("friend list:", friendData);
       // TODO: save list to global list, and map the results in the friends list tab
@@ -177,45 +179,9 @@ export default function Profile({ user }) {
     }
   };
 
-  // List liked places / Favorites
-  const getPlaceInfo = (place_id) => {
-    // // Create a new instance of the Loader
-    // const loader = new Loader({
-    //   apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API,
-    //   version: "weekly",
-    //   libraries: ["places"],
-    // });
-
-    // // Load the Google Maps API and fetch place details
-    // loader.load().then(() => {
-    //   const { google } = window;
-    //   const service = new google.maps.places.PlacesService(
-    //     document.createElement("div")
-    //   );
-
-    //   // Fetch the place details using the place_id
-    //   service.getDetails({ place_id }, (placeResult, status) => {
-    //     if (status === google.maps.places.PlacesServiceStatus.OK) {
-    //       // Access the place details in placeResult
-    //       console.log("Place Details:", placeResult);
-    //     } else {
-    //       console.error("Failed to fetch place details:", status);
-    //     }
-    //   });
-    // });
-  };
-
-  const getLikedPlaces = () => {
-    console.log("liked places userData:", userData.favorites);
-    const promises = userData.favorites
-      ? userData.favorites.map((placeId) => getPlaceInfo(placeId))
-      : [];
-  };
-
   useEffect(() => {
     if (userData && userData.friends) {
       createFriendList(userData.friends);
-      getLikedPlaces();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userData]);
@@ -236,6 +202,8 @@ export default function Profile({ user }) {
     console.log("Loading Profile...");
     return (
       <div className="userProfile">
+        {/* <meta name="viewport" content="initial-scale=1, width=device-width" /> */}
+
         <Sheet>
           <Stack direction="row" alignItems="center" spacing={0}>
             {/* Home Button */}
@@ -331,14 +299,7 @@ export default function Profile({ user }) {
               {/* Render the matching users */}
             </Stack>
             {alertMessage && (
-              <Box
-                sx={{
-                  display: "flex",
-                  gap: 2,
-                  width: "100%",
-                  flexDirection: "column",
-                }}
-              >
+              <Box sx={{ display: 'flex', gap: 2, width: '100%', flexDirection: 'column' }}>
                 <Alert variant="solid" size="md" color={alertStatus}>
                   {alertMessage}
                 </Alert>
@@ -374,54 +335,7 @@ export default function Profile({ user }) {
 
               {/* TabPanel for User's like places */}
               <TabPanel value={0} sx={{ p: 2 }}>
-                <div>
-                  <List
-                    variant="outlined"
-                    sx={{
-                      bgcolor: "background.body",
-                      minWidth: 240,
-                      borderRadius: "sm",
-                      boxShadow: "sm",
-                      "--ListItemDecorator-size": "48px",
-                      "--ListItem-paddingLeft": "1.5rem",
-                      "--ListItem-paddingRight": "1rem",
-                    }}
-                  >
-                    {friendsList.map((friend, index) => (
-                      <Fragment key={index}>
-                        <Button
-                          variant="plain"
-                          sx={{
-                            display: "flex",
-                            justifyContent: "flex-start",
-                            alignItems: "center",
-                            width: "100%",
-                            padding: 0,
-                          }}
-                          component="li"
-                          // route to friends-profile page
-                          onClick={handleFriendProfile}
-                        >
-                          <ListItem key={index}>
-                            <ListItemDecorator sx={{ alignSelf: "flex-start" }}>
-                              <Avatar size="sm" src="" />
-                            </ListItemDecorator>
-                            <Typography
-                              color="black"
-                              sx={{
-                                fontWeight: "normal",
-                                marginLeft: "0.5rem",
-                              }}
-                            >
-                              {friend}
-                            </Typography>
-                          </ListItem>
-                        </Button>
-                        {index !== friendsList.length - 1 && <Divider />}
-                      </Fragment>
-                    ))}
-                  </List>
-                </div>
+                Like Places tab panel
               </TabPanel>
 
               {/* TabPanel for User's friends list */}
@@ -430,33 +344,35 @@ export default function Profile({ user }) {
                   <List
                     variant="outlined"
                     sx={{
-                      bgcolor: "background.body",
+                      bgcolor: 'background.body',
                       minWidth: 240,
-                      borderRadius: "sm",
-                      boxShadow: "sm",
-                      "--ListItemDecorator-size": "48px",
-                      "--ListItem-paddingLeft": "1.5rem",
-                      "--ListItem-paddingRight": "1rem",
+                      borderRadius: 'sm',
+                      boxShadow: 'sm',
+                      '--ListItemDecorator-size': '48px',
+                      '--ListItem-paddingLeft': '1.5rem',
+                      '--ListItem-paddingRight': '1rem',
                     }}
                   >
                     {friendsList.map((friend, index) => (
-                      <Fragment key={index}>
-                        <ListItem key={index} onClick={handleFriendProfile}>
-                          <ListItemDecorator sx={{ alignSelf: "flex-start" }}>
-                            <Avatar size="sm" src="" />
-                          </ListItemDecorator>
-                          <Typography
-                            color="black"
-                            sx={{
-                              fontWeight: "normal",
-                              marginLeft: "0.5rem",
-                            }}
+                      <React.Fragment key={index}>
+                        <Button variant="plain"
+                          sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', width: '100%', padding: 0 }}
+                          component="li"
+                          disableRipple
+                          // route to friends-profile page
+                          onClick={handleFriendProfile(friend)}
                           >
-                            {friend}
-                          </Typography>
-                        </ListItem>
+                          <ListItem key={index}>
+                            <ListItemDecorator sx={{ alignSelf: 'flex-start' }}>
+                              <Avatar size="sm" src="/static/images/avatar/1.jpg" />
+                            </ListItemDecorator>
+                            <Typography color="black" sx={{ fontWeight: 'normal', marginLeft: '0.5rem' }}>
+                              {friend}
+                            </Typography>
+                          </ListItem>
+                        </Button>
                         {index !== friendsList.length - 1 && <Divider />}
-                      </Fragment>
+                      </React.Fragment>
                     ))}
                   </List>
                 </div>
@@ -464,7 +380,7 @@ export default function Profile({ user }) {
             </Tabs>
           </Sheet>
         </Sheet>
-      </div>
+      </div >
     );
   }
 }
